5af8b588dce44440ca7a4bc9c74e9d28eaeef1e4
Tasio Almansa <at.almansa@gmail.com>
2018-03-02 11:50:54 +0100

    Versión 1.0 - Se completa la localización del usuario y se implementa una forma rápida de añadir lugares nuevos. Queda la persistencia de datos en servidor y una personalización más detallada de la creación del lugar. Sin embargo se dan por cumplidos los objetivos del proyecto.

--- LugaresInteres/ViewController.swift
+++ LugaresInteres/ViewController.swift
@@ -11,21 +11,29 @@ import MapKit
 import CoreLocation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
     @IBOutlet var mapView: MKMapView!
     
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        mapView.setRegion(region,animated: true)
+        mapView.showsUserLocation = true
+    }
     var lugares : [Lugar] = []
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.mapView.delegate = self
-        //mapView.userLocation.title = "Ubicación Actual"
-                
-        mapView.showsUserLocation = true
-        // creacion de los datos**********************
+        
+        
         
         var lugar = Lugar(nombre: "Bar Manolo", cat: "Bar", direccion: "Calle de Cartaya, 1, 21002 Huelva, España")
         
@@ -60,23 +68,91 @@ class ViewController: UIViewController {
                     }
                 }
             }
-            
         } //end for in
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.mapView.delegate = self
+        //mapView.userLocation.title = "Ubicación Actual"
+                
+        //mapView.showsUserLocation = true
+        // creacion de los datos**********************
         
+        
+            
+    
+     manager.stopUpdatingLocation()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
   
+    @IBAction func bnuevoLugar(_ sender: UIButton) {
+        
+        
+        //let geocoder = CLGeocoder()
+         let annotation = MKPointAnnotation()
+        switch sender.tag {
+        case 1:
+           // print(mapView.userLocation)
+            annotation.subtitle = "Bar"
+            break
+        case 2:
+            annotation.subtitle = "Concierto"
+            break
+            
+        case 3:
+            annotation.subtitle = "Hotel"
+            break
+            
+        case 4:
+            annotation.subtitle = "Café"
+            break
+         
+        case 5:
+            annotation.subtitle = "Taxi"
+            break
+        case 6:
+            annotation.subtitle = "Museo"
+            break
+        case 7:
+            annotation.subtitle = "Bus"
+            break
+        case 8:
+            annotation.subtitle = "Teatro"
+            break
+        case 9:
+            annotation.subtitle = "Parque"
+            break
+        
+        default:
+            break
+            
+        }
+        annotation.title = "Nuevo Lugar"
+        
+        annotation.coordinate = (mapView.userLocation.location?.coordinate)!
+        
+        
+        self.mapView.showAnnotations([annotation], animated: true)
+        self.mapView.selectAnnotation(annotation, animated: true)
+        
+        let alert = UIAlertController(title: "Nuevo lugar añadido!", message: "Has añadido un nuevo lugar de interés!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK!", style: .default))
+        self.present(alert,animated: true)
+    }
 }
 
 extension ViewController : MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         let identifier = "myPin"
         
+            
         if annotation.isKind(of: MKUserLocation.self)
         {
             return nil
@@ -107,7 +183,22 @@ extension ViewController : MKMapViewDelegate {
             imageView.image = #imageLiteral(resourceName: "bus")
             annotationView?.pinTintColor = UIColor.blue
         }
-        
+        if( annotation.subtitle as! String  == "Concierto"){
+            imageView.image = #imageLiteral(resourceName: "concierto")
+            annotationView?.pinTintColor = UIColor.brown
+        }
+        if( annotation.subtitle as! String  == "Hotel"){
+            imageView.image = #imageLiteral(resourceName: "hotel")
+            annotationView?.pinTintColor = UIColor.blue
+        }
+        if( annotation.subtitle as! String  == "Café"){
+            imageView.image = #imageLiteral(resourceName: "cafe")
+            annotationView?.pinTintColor = UIColor.brown
+        }
+        if( annotation.subtitle as! String  == "Parque"){
+            imageView.image = #imageLiteral(resourceName: "parque")
+            annotationView?.pinTintColor = UIColor.green
+        }
         annotationView?.leftCalloutAccessoryView = imageView
         
         
